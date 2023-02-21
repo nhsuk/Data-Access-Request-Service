3282c285f1acbacb75c35ee9dc8803c9866dce69
mmips <116642254+mmips@users.noreply.github.com>
2023-02-21 09:13:56 +0000

    js update

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,20 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+  // Commercial example
+  router.post('/why-you-are-applying/commercial', (req, res) => {
+    // Make a variable and give it the value from 'know-nhs-number'
+    const value = req.session.data['example'];
+  
+    // Check whether the variable matches a condition
+    if (value === 'yes') {
+      // Send user to next page
+      res.redirect('/why-you-are-applying/commercial-yes');
+    } else {
+      // Send user to ineligible page
+      res.redirect('/why-you-are-applying/outputs');
+    }
+  });
+  
+
 module.exports = router;
